53e6fe149a1778462b10f01c48e3efe5b3f3e6dd
Tom Strong <strng_thms@yahoo.com>
2022-07-16 12:25:43 -0400

    Added Header component, base styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="container">
-      <h1>FindMeABeer</h1>
-      <h2>Hello Beer World!</h2>
+    <div className='container'>
+      <Header />
     </div>
   );
 }
