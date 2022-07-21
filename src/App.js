8cab4377db409f44e4b84d5e4ab4eea031995cbf
Tom Strong <strng_thms@yahoo.com>
2022-07-20 20:20:38 -0400

    Deleted mock data, comment some console logs

--- src/App.js
+++ src/App.js
@@ -4,63 +4,24 @@ import Venues from './components/Venues';
 import SearchForm from './components/SearchForm';
 
 function App() {
-  const [venues, setVenues] = useState(
-    []
-    // Mock data for setting up and testing layout
-    // [
-    //   {
-    //     id: 1,
-    //     name: 'Test Brewery',
-    //     address: '1234 Alphabet St.',
-    //     city: 'Louisville',
-    //     state: 'KY',
-    //     zipcode: '402XX',
-    //     phone: '502-502-5025',
-    //   },
-    //   {
-    //     id: 2,
-    //     name: 'Venue du Test',
-    //     address: '567 Another St.',
-    //     city: 'Louisville',
-    //     state: 'KY',
-    //     zipcode: '402XX',
-    //     phone: '502-205-2055',
-    //   },
-    //   {
-    //     id: 3,
-    //     name: 'Madeup Brewpub',
-    //     address: '999 Industry Circle',
-    //     city: 'Louisville',
-    //     state: 'KY',
-    //     zipcode: '402XX',
-    //     phone: '502-555-9999',
-    //   },
-    // ]
-  );
-
-  // const [fetchURL, setFetchURL] = useState('');
   const [topVenues, setTopVenues] = useState([]);
 
-  // const [search, setSearch] = useState(false);
-
   let fetchURL = '';
-  // let topVenues;
   let venueData;
 
   const getVenueURL = (venue) => {
-    console.log(venue.type, venue.term);
+    // console.log(venue.type, venue.term);
 
     fetchURL = `https://beermapping.com/webservice/loc${venue.type}/fdcfda52f27a87308dfd30e9647f4905/${venue.term}&s=json`;
-    console.log(fetchURL);
+    // console.log(fetchURL);
 
     const fetchVenues = async () => {
-      console.log('fetching venues');
-      console.log(fetchURL);
+      // console.log('fetching venues');
+      // console.log(fetchURL);
       const res = await fetch(fetchURL);
 
       venueData = await res.json();
 
-      console.log(venueData.slice(0, 5));
       setTopVenues(venueData.slice(0, 5));
 
       return venueData;
--- src/components/Venue.js
+++ src/components/Venue.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+
 const Venue = ({ venue }) => {
   return (
     <div id='protoVenue' className='protoVenue'>
       <h2 key={venue.id}>{venue.name}</h2>
-      <p>{venue.address}</p>
+      <p>{venue.street}</p>
       <p>
-        {venue.city}, {venue.state} {venue.zipcode}
+        {venue.city}, {venue.state} {venue.zip}
       </p>
       <p>{venue.phone}</p>
+      <a href='#' target='_blank'>
+        {venue.url}
+      </a>
+      {/* <iframe src={venue.proxylink}></iframe>    this is not quite right*/}
     </div>
   );
 };
